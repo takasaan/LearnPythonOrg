bbfb0c57f52701b8d4391c577f4cabbc5b443f80
takasaan <ms.mihotakahashi@gmail.com>
2024-04-07 00:19:01 +0900

    test

---
+++ 0307_leetcode.py
@@ -0,0 +1,27 @@
+#input 
+#nums = [a, b, c, d]
+#target = q
+
+#check : a + b == q
+#check : a + c == q
+#check: a + d == q
+
+#check: b + c == q
+#check: b + d == q
+
+#check: c = d == q
+
+for i in range(len(nums)):
+
+#return len(nums):
+
+    if arr[0]+arr[1] == q:
+        
+nums = [5,6,7] #return len(nums):
+#target = 13のとき 
+target = 13
+for i in range(len(nums)):#右にあるnumsのindexが i に代入されて、print[i]として1つずつ出力される
+    print(nums[i]) #鉤括弧しているから[i]
+    
+    arr = [1, 2, 3, 4]
+print(arr[0]+arr[1])
